8dc1d5b6ae17c1295a961a5bf4dcac052aab21d2
Frank van den Thillart <thifm@hr.nl>
2022-06-07 16:15:26 +0200

    Tomcat container werkt. Met mysql data in data directory

--- Print-dockerfiles.ps1
+++ Print-dockerfiles.ps1
@@ -1,1 +1,3 @@
 Get-ChildItem -Recurse -File -Path .\*docker* | Where-Object {$_.Name -inotlike '*.ps1'} | ForEach-Object {"`r`n*** $($_.Directory.Name)/$($_.Name) ***`r`n";Get-Content $_.FullName}
+$item = Get-Item -Path '.\readme.md' 
+"`r`n*** $($item.Directory.Name)/$($item.Name) ***`r`n";Get-Content $item.FullName
